5940ffc7a9860d5d19480e1780298c220cbaf88d
isabelgs26 <MIsabel.Garcia28@alu.uclm.es>
2025-10-07 17:31:47 +0200

    Implementar Bloque 4: Exponer lógica en capa REST

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
 const fs = require("fs");
 const express = require('express');
 const app = express();
-const modelo = require("./servidor/modelo.js");
+const modelo = require('./servidor/modelo.js');
+
 const PORT = process.env.PORT || 3000;
+
+// Crear instancia del sistema
+let sistema = new modelo.Sistema();
+
 app.use(express.static(__dirname + "/"));
+
 app.get("/", function (request, response) {
     var contenido = fs.readFileSync(__dirname + "/cliente/index.html");
     response.setHeader("Content-type", "text/html");
     response.send(contenido);
 });
+
+app.get("/agregarUsuario/:nick", function (request, response) {
+    let nick = request.params.nick;
+    let res = sistema.agregarUsuario(nick);  // ← CORREGIDO: sistema.agregarUsuario
+    response.send(res);
+});
+
 app.listen(PORT, () => {
     console.log(`App está escuchando en el puerto ${PORT}`);
     console.log('Ctrl+C para salir');
-}); 
+});
--- servidor/modelo.js
+++ servidor/modelo.js
@@ -2,40 +2,44 @@ function Sistema() {
     this.usuarios = {};
     this.contadorId = 1;
 
-    this.agregarUsuario = function (nick, email, password) {
-        this.usuarios[nick] = new Usuario(nick, email, password); this.contadorId++;
-        return this.contadorId;
-    }
-    this.obtenerUsuario = function () {
-        return this.usuarios;
-    }
-    this.usuarioActivo = function (nick) {
-        if (this.usuarios[nick]) {
-            return true;
+    this.agregarUsuario = function (nick) {
+        let res = { "nick": -1 };
+        if (!this.usuarios[nick]) {
+            this.usuarios[nick] = new Usuario(nick);
+            res.nick = nick;
         } else {
-            return false;
+            console.log("el nick " + nick + " está en uso");
         }
-    }
-    this.eliminarUsuario = function (nick) {
-        if (this.usuarios.hasOwnProperty(nick)) {
-            delete this.usuarios[nick];
-            return true;
-        }
-        return false;
-    }
-
-    this.numeroUsuarios = function () {
-        return Object.keys(this.usuarios).length;
+        return res;
     }
 
     this.obtenerUsuarios = function () {
         return this.usuarios;
     }
+
+    this.usuarioActivo = function (nick) {
+        let res = { "activo": false };
+        if (this.usuarios[nick]) {
+            res.activo = true;
+        }
+        return res;
+    }
+
+    this.eliminarUsuario = function (nick) {
+        let res = { "eliminado": false };
+        if (this.usuarios[nick]) {
+            delete this.usuarios[nick];
+            res.eliminado = true;
+        }
+        return res;
+    }
+
+    this.numeroUsuarios = function () {
+        return Object.keys(this.usuarios).length;
+    }
 }
 
-function Usuario(nick, email, password) {
+function Usuario(nick) {
     this.nick = nick;
-    this.email = email;
-    this.password = password;
 }
-module.exports.Sistema = Sistema
+module.exports.Sistema = Sistema;
