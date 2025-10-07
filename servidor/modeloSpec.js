f34f4fcf91210114f37f245359f1a0cae2444bda
isabelgs26 <MIsabel.Garcia28@alu.uclm.es>
2025-10-07 17:05:48 +0200

    Resolver conflictos

---
+++ servidor/modeloSpec.js
@@ -0,0 +1,31 @@
+const modelo = require("./modelo.js");
+describe('El sistema', function () {
+  let sistema;
+
+  beforeEach(function () {
+    sistema = new Sistema();
+  });
+
+  it('agregar usuario', function () {
+    sistema.agregarUsuario('juan', 'juan', '1234');
+    expect(sistema.numeroUsuarios()).toEqual(1);
+  });
+
+  it('eliminar usuario', function () {
+    sistema.agregarUsuario('juan', 'juan', '1234');
+    sistema.eliminarUsuario('juan');
+    expect(sistema.numeroUsuarios()).toEqual(0);
+  });
+
+  it('obtener usuario', function () {
+    sistema.agregarUsuario('juan', 'juan', '1234');
+    const usuarios = sistema.obtenerUsuarios();
+    expect(usuarios['juan']).toBeDefined();
+  });
+
+  it('usuario activo', function () {
+    sistema.agregarUsuario('juan', 'juan', '1234');
+    expect(sistema.usuarioActivo('juan')).toBeTrue();
+    expect(sistema.usuarioActivo('pedro')).toBeFalse();
+  });
+});
