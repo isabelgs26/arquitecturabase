6d82ce4df885d4da335b1821eef5495b031de10a
isabelgs26 <MIsabel.Garcia28@alu.uclm.es>
2025-10-08 13:08:01 +0200

    Bloque 4: añadir rutas REST usuarioActivo, numeroUsuarios y eliminarUsuario

--- index.js
+++ index.js
@@ -18,8 +18,31 @@ app.get("/", function (request, response) {
 
 app.get("/agregarUsuario/:nick", function (request, response) {
     let nick = request.params.nick;
-    let res = sistema.agregarUsuario(nick);  // ← CORREGIDO: sistema.agregarUsuario
-    response.send(res);
+    let res = sistema.agregarUsuario(nick);
+    response.json(res);
+});
+
+
+app.get("/obtenerUsuarios", function (request, response) {
+    let res = sistema.obtenerUsuarios();
+    response.json(res);
+});
+
+app.get("/usuarioActivo/:nick", function (request, response) {
+    let nick = request.params.nick;
+    let activo = sistema.usuarioActivo(nick);
+    response.json({ nick: nick, activo: activo });
+});
+
+app.get("/numeroUsuarios", function (request, response) {
+    let num = sistema.numeroUsuarios();
+    response.json({ numeroUsuarios: num });
+});
+
+app.get("/eliminarUsuario/:nick", function (request, response) {
+    let nick = request.params.nick;
+    let res = sistema.eliminarUsuario(nick);
+    response.json(res);
 });
 
 app.listen(PORT, () => {
